be5f9c829c04c64c547ff8d9bfab0c824e5f0d09
Roberto Lillo <robertoxlp@hotmail.com>
2018-11-11 16:34:33 -0300

    avance

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # EDALab1
 Lab 1 EDA C (2-2018)
 
+    - Avance 11 de Noviembre 2018:
+    
--- punteros.c
+++ punteros.c
@@ -8,14 +8,17 @@
 
 int main () {
 
+    // Formas basicas de punteros
     int numero = 40;
-    int *pNumero = &numero;
+    int * pNumero = &numero;
 
-    printf("\nDireccion \t Nombre \t Valor \n\n");
+    printf("Direccion \t Nombre \t Valor \n\n");
     printf("%p \t %s \t %d \n", &numero, "numero", numero);     // Direccion
     printf("%p \t %s \t %d \n\n", pNumero, "numero", *pNumero); // Dereferenciar
+    // -------------------------------------------------------------------------------------------------------
 
 
+    // Punteros y arreglos
     int i;
     int arreglo[3] = {2, 3, 4};
 
@@ -24,7 +27,20 @@ int main () {
     }
 
     printf("\narreglo \t %p \t %d", arreglo, *arreglo); // Nombres de arreglos son punteros al primer elemento
-    printf("\n(arreglo+2) \t %p \t %d \n", arreglo, *(arreglo+2));
+    printf("\n(arreglo+2) \t %p \t %d \n\n", arreglo, *(arreglo+2));
+    // -------------------------------------------------------------------------------------------------------
+
+
+    // Punteros y strings
+    char juego1[] = "Red Dead Redemption";      // juego1 es una constante y no se puede cambiar.
+    char * juego2 = "Red Dead Redemption 2";    // Al hacer juego2 un puntero, es variable.
+    // juego2 apunta solamente al primer elemento del string.
+
+    // juego1 = "Crackdown";    **ERROR**
+    puts(juego2); // Imprime cada char hasta encontrar un null (similar al concepto de listas enlazadas).
+    juego2 = "Crackdown 2";
+    puts(juego2);
+    // -------------------------------------------------------------------------------------------------------
 
     return 0;
 }
