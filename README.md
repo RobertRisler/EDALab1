dd58c860272a571f7d301f3c74b774a2080efd00
Roberto Lillo <robertoxlp@hotmail.com>
2018-11-11 22:43:03 -0300

    Avance 11 de Noviembre del 2018

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # EDALab1
 Lab 1 EDA C (2-2018)
 
-    - 11 de Noviembre 2018:
+    Avance 11 de Noviembre 2018:
+
+        - Lee archivo maquina.in:
+            · Obtiene valores para alto y ancho de la máquina.
+            · Crea matriz máquina a partir de esos valores.
+            · Rellena la matriz máquina con los valores del archivo.
--- lab.c
+++ lab.c
@@ -3,45 +3,48 @@
 #include <string.h>
 #include "lab.h"
 
-void crearMatriz ();
-
 int main () {
 
-    crearMatriz();
-
-    //PRUEBAS
-
-
-    //
-    
-    return 0;
-}
-
-void crearMatriz () {
-
-    int i;
+    int i, j;
     int dimensiones[2];
-    char linea[100];
+    char linea1[10];
     char *token;
 
     FILE *pArchivo;
     pArchivo = fopen("maquina.in", "r");
-    fgets(linea, 10, pArchivo);
+    fgets(linea1, 10, pArchivo);
 
     // Obtencion de las dimensiones de la maquina.
-    token = strtok(linea, " ");
-    dimensiones[0] = atoi(token);
-    token = strtok(NULL, " ");
-    dimensiones[1] = atoi(token);
-
-    printf("%d %d", dimensiones[0], dimensiones[1]);
+    dimensiones[0] = atoi(strtok(linea1, " "));
+    dimensiones[1] = atoi(strtok(NULL, " "));
 
     // Creacion matriz.
-    char maquina[dimensiones[0]][dimensiones[1]];
-    
+    char linea2[(2 * dimensiones[1]) + 1];          // Por los espacios, el largo es (2 * Ancho), + 1 por el \0.
+    char maquina[dimensiones[0]][dimensiones[1]];   // Alto X Ancho
+    int contador1 = 0;
+    int contador2;
 
-    
+    while (!feof(pArchivo)) {   // feof = end of file.
+        fgets(linea2, (2 * dimensiones[1] + 1), pArchivo);
+        token = strtok(linea2, " ");
+        contador2 = 0;
+        while (token != NULL) {
+            maquina[contador1][contador2] = token[0];
+            token = strtok(NULL, " ");
+            contador2++;
+        }
+        contador1++;
+    }
 
     fclose(pArchivo);
-    return;
+
+    // Solo para ver la matriz.
+    for (i = 0; i < dimensiones[0]; i++) {
+        for (j = 0; j < dimensiones[1]; j++) {
+            printf("%c ", maquina[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
 }
